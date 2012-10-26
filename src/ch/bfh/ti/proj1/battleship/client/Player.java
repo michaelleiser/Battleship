79a66e88663da03d42d24b961270c9b26c84b77a
kotld1 <kotld1@bfh.ch>
2012-10-26 08:00:51 -0700

    commit without empty packages

---
+++ src/ch/bfh/ti/proj1/battleship/client/Player.java
@@ -0,0 +1,11 @@
+package ch.bfh.ti.proj1.battleship.client;
+
+/**
+ * One of the two players who plays the battleship game.
+ * 
+ * @author Daniel Kotlàris
+ * @author Michael Leiser
+ */
+public class Player {
+
+}
---
+++ src/ch/bfh/ti/proj1/battleship/server/EntityManager.java
@@ -0,0 +1,21 @@
+package ch.bfh.ti.proj1.battleship.server;
+
+import ch.bfh.ti.proj1.battleship.client.Player;
+
+/**
+ * The Entity Manager is responsible to store 
+ * all important data for the battleship game.
+ * 
+ * @author Daniel Kotlàris
+ * @author Michael Leiser
+ */
+public class EntityManager {
+
+	// the player on the same computer as the host
+	Player localPlayer;
+	// the second player who connects to the server from a remote device
+	Player remotePlayer;
+	
+	// TODO: Add more entities...
+	
+}
